1ba7fd8e050f0c0d72d5c89977a1d567dedd3a8b
wenye <1583247975@qq.com>
2019-10-24 23:12:37 +0800

    feat: 初步完成代码

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,131 @@
-export function hello() {
-  return "hello world!";
+import { Redis } from "ioredis";
+
+export interface IOptions {
+  /** ioredis实例 */
+  client: Redis;
+  /** 键前缀 */
+  keyPrefix: string;
+}
+
+export default class {
+  private keyPrefix: string;
+  private redis: Redis;
+
+  constructor(options: IOptions) {
+    this.redis = options.client;
+    this.keyPrefix = options.keyPrefix;
+    // 初始化lua脚本
+    this.initLua();
+  }
+
+  private getKeyName(lockNmae: string) {
+    return `${this.keyPrefix}${lockNmae}`;
+  }
+
+  private getIdentifier() {
+    return String(Math.random()).split(".")[1];
+  }
+
+  private initLua() {
+    // 释放锁
+    this.redis.defineCommand("releaseLock", {
+      numberOfKeys: 1,
+      lua: `
+        if (redis.call("get", KEYS[1]) == ARGV[1])
+        then
+          return redis.call("del", KEYS[1]);
+        end
+      `,
+    });
+    // 获取信号量
+    this.redis.defineCommand("acquireSemaphore", {
+      numberOfKeys: 1,
+      lua: `
+        redis.call("zremrangebyscore", KEYS[1], "-inf", ARGV[1]);
+        if (redis.call("zcard", KEYS[1]) < tonumber(ARGV[2]))
+        then
+          redis.call("zadd", KEYS[1], ARGV[3], ARGV[4]);
+          return ARGV[4];
+        end
+      `,
+    });
+    // 释放信号量
+    this.redis.defineCommand("refreshSemaphore", {
+      numberOfKeys: 1,
+      lua: `
+        if redis.call("zscore", KEYS[1], ARGV[1])
+        then
+          return redis.call("zadd", KEYS[1], ARGV[2], ARGV[1]);
+        end
+      `,
+    });
+  }
+
+  /**
+   * 请求一个锁标志
+   * @param lockName 锁名字
+   * @param acquireTimeout 请求超时毫秒数，默认3000
+   * @param lockTimeout 锁过期毫秒数，默认3000
+   */
+  async acquireLock(lockName: string, acquireTimeout: number = 3000, lockTimeout: number = 3000) {
+    lockName = this.getKeyName(lockName);
+    lockTimeout = Math.ceil(lockTimeout); // 超时时间都是整数
+    const identifier = this.getIdentifier();
+    const end = Date.now() + acquireTimeout;
+    while (Date.now() < end) {
+      // 设置锁且包含过期时间
+      const ret = await this.redis.set(lockName, identifier, "PX", lockTimeout, "NX");
+      if (ret === "OK") return identifier;
+    }
+    return null;
+  }
+
+  /**
+   * 释放锁
+   * @param lockName 锁名字
+   * @param identifier 锁标志
+   */
+  async releaseLock(lockName: string, identifier: string) {
+    lockName = this.getKeyName(lockName);
+    const ret = await (this.redis as any).releaseLock(lockName, identifier);
+    return ret === 1;
+  }
+
+  /**
+   * 获取信号量
+   * @param sename 信号量名字
+   * @param limit 限制数
+   * @param timeout 信号量过期毫秒数，默认3000
+   */
+  async acquireSemaphore(sename: string, limit: number, timeout: number = 3000) {
+    sename = this.getKeyName(sename);
+    const identifier = this.getIdentifier();
+    const now = Date.now();
+    const ident = await (this.redis as any).acquireSemaphore(sename, now - timeout, limit, now, identifier);
+    return ident;
+  }
+
+  /**
+   * 释放信号量
+   * @param sename 信号量名字
+   * @param identifier 信号量标识
+   */
+  async releaseSemaphore(sename: string, identifier: string) {
+    sename = this.getKeyName(sename);
+    // 正确释放返回true 否则就是该信号量已过期
+    const ret = await this.redis.zrem(sename, identifier);
+    return Boolean(ret);
+  }
+
+  /**
+   * 刷新信号量
+   * @param sename 信号量名字
+   * @param identifier 信号量标识符
+   */
+  async refreshSemaphore(sename: string, identifier: string) {
+    sename = this.getKeyName(sename);
+    const now = Date.now();
+    const ret = await (this.redis as any).refreshSemaphore(sename, identifier, now);
+    return ret === 0;
+  }
 }
--- test/test-index.ts
+++ test/test-index.ts
@@ -1,9 +1,49 @@
-import { hello } from "../src/index";
+import redisLock from "../src/index";
+import Redis from "ioredis";
 import { assert } from "chai";
 
+const redis = new Redis({
+  host: "127.0.0.1",
+  port: 6379,
+});
+
+const rlock = new redisLock({
+  client: redis,
+  keyPrefix: "mt:",
+});
+
 describe("test", function() {
-  it("index", function() {
-    const ret = hello();
-    assert.equal(ret, "hello world!");
+  describe("lock", function() {
+    let lockName = "tlock";
+    let identifier: string | null = null;
+    it("acquireLock", async function() {
+      identifier = await rlock.acquireLock(lockName);
+      assert.notStrictEqual(identifier, null);
+    });
+    it("releaseLock", async function() {
+      const success = await rlock.releaseLock(lockName, identifier as string);
+      assert.strictEqual(success, true);
+    });
+  });
+
+  describe("acquireSemaphore", function() {
+    let sename = "tsename";
+    let identifier: string | null = null;
+    it("acquireSemaphore", async function() {
+      // 第一个可以拿到
+      identifier = await rlock.acquireSemaphore(sename, 1);
+      assert.notStrictEqual(identifier, null);
+      // 第二个拿不到
+      const ident = await rlock.acquireSemaphore(sename, 1);
+      assert.strictEqual(ident, null);
+    });
+    it("refreshSemaphore", async function() {
+      const success = await rlock.refreshSemaphore(sename, identifier as string);
+      assert.strictEqual(success, true);
+    });
+    it("releaseSemaphore", async function() {
+      const success = await rlock.releaseSemaphore(sename, identifier as string);
+      assert.strictEqual(success, true);
+    });
   });
 });
